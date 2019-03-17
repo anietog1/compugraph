6b5584c1f47e7596a1d473d3be2da9d428bbad47
Agustín Nieto García <anietog1@eafit.edu.co>
2019-03-17 14:46:43 -0500

    Adding simplified version of utilities3d. The bug still persists.

---
+++ lib/simple3d/Matrix4x4.java
@@ -0,0 +1,166 @@
+package simple3d;
+
+import java.util.Arrays;
+
+public class Matrix4x4 {
+    public final double matrix[][];
+
+    public Matrix4x4(double matrix[][]) {
+        this.matrix = matrix;
+    }
+
+    public static Matrix4x4 zero() {
+        double matrix[][] = {
+            {0, 0, 0, 0},
+            {0, 0, 0, 0},
+            {0, 0, 0, 0},
+            {0, 0, 0, 0}
+        };
+
+        return new Matrix4x4(matrix);
+    }
+
+    public static Matrix4x4 identity() {
+        double matrix[][] = {
+            {1, 0, 0, 0},
+            {0, 1, 0, 0},
+            {0, 0, 1, 0},
+            {0, 0, 0, 1}
+        };
+
+        return new Matrix4x4(matrix);
+    }
+
+    public Matrix4x4 plus(Matrix4x4 m) {
+        Matrix4x4 result = Matrix4x4.zero();
+
+        for (int i = 0; i < 4; ++i) {
+            for (int j = 0; j < 4; ++j) {
+                result.matrix[i][j] = matrix[i][j] + m.matrix[i][j];
+            }
+        }
+
+        return result;
+    }
+
+    public Vector3 times(Vector3 p) {
+        double x = matrix[0][0] * p.x + matrix[0][1] * p.y + matrix[0][2] * p.z + matrix[0][3];
+        double y = matrix[1][0] * p.x + matrix[1][1] * p.y + matrix[1][2] * p.z + matrix[1][3];
+        double z = matrix[2][0] * p.x + matrix[2][1] * p.y + matrix[2][2] * p.z + matrix[2][3];
+        double w = matrix[3][0] * p.x + matrix[3][1] * p.y + matrix[3][2] * p.z + matrix[3][3];
+
+        return new Vector3(x, y, z, w);
+    }
+
+    public Matrix4x4 times(Matrix4x4 m) {
+        Matrix4x4 result = Matrix4x4.zero();
+
+        for (int i = 0; i < 4; ++i) {
+            for (int j = 0; j < 4; ++j) {
+                for (int k = 0; k < 4; ++k) {
+                    result.matrix[i][j] += matrix[i][k] * m.matrix[k][j];
+                }
+            }
+        }
+
+        return result;
+    }
+
+    public Matrix4x4 times(double n) {
+        Matrix4x4 result = Matrix4x4.zero();
+
+        for (int i = 0; i < 4; ++i) {
+            for (int j = 0; j < 4; ++j) {
+                result.matrix[i][j] = matrix[i][j] * n;
+            }
+        }
+
+        return result;
+    }
+
+    public static Matrix4x4 translate(double dx, double dy, double dz) {
+        double matrix[][] = {
+            {1, 0, 0, dx},
+            {0, 1, 0, dy},
+            {0, 0, 1, dz},
+            {0, 0, 0, 1}
+        };
+
+        return new Matrix4x4(matrix);
+    }
+
+    public static Matrix4x4 scale(double sx, double sy, double sz) {
+        double matrix[][] = {
+            {sx, 0, 0, 0},
+            {0, sy, 0, 0},
+            {0, 0, sz, 0},
+            {0, 0, 0, 1}
+        };
+
+        return new Matrix4x4(matrix);
+    }
+
+    public static Matrix4x4 rotateX(double dtheta) {
+        double matrix[][] = {
+            {1, 0, 0, 0},
+            {0, Math.cos(dtheta), -Math.sin(dtheta), 0},
+            {0, Math.sin(dtheta), Math.cos(dtheta), 0},
+            {0, 0, 0, 1}
+        };
+
+        return new Matrix4x4(matrix);
+    }
+
+    public static Matrix4x4 rotateY(double dtheta) {
+        double matrix[][] = {
+            {Math.cos(dtheta), 0, Math.sin(dtheta), 0},
+            {0, 1, 0, 0},
+            {-Math.sin(dtheta), 0, Math.cos(dtheta), 0},
+            {0, 0, 0, 1}
+        };
+
+        return new Matrix4x4(matrix);
+    }
+
+    public static Matrix4x4 rotateZ(double dtheta) {
+        double matrix[][] = {
+            {Math.cos(dtheta), -Math.sin(dtheta), 0, 0},
+            {Math.sin(dtheta), Math.cos(dtheta), 0, 0},
+            {0, 0, 1, 0},
+            {0, 0, 0, 1}
+        };
+
+        return new Matrix4x4(matrix);
+    }
+
+    public static Matrix4x4 perspective(double d) {
+        double matrix[][] = {
+            {1, 0, 0, 0},
+            {0, 1, 0, 0},
+            {0, 0, 1, 0},
+            {0, 0, 1 / d, 0}
+        };
+
+        return new Matrix4x4(matrix);
+    }
+
+    public static Matrix4x4 lookAt(Vector3 from, Vector3 to, Vector3 orientation) {
+        Vector3 n = to.minus(from).normalized();
+        Vector3 u = orientation.cross(n).normalized();
+        Vector3 v = n.cross(u);
+
+        double matrix[][] = {
+            {u.x, u.y, u.z, -u.dot(from)},
+            {v.x, v.y, v.z, -v.dot(from)},
+            {n.x, n.y, n.z, -n.dot(from)},
+            {0, 0, 0, 1}
+        };
+
+        return new Matrix4x4(matrix);
+    }
+
+    @Override
+    public String toString() {
+        return "Matrix4x4{" + "matrix=" + Arrays.deepToString(matrix) + '}';
+    }
+}
---
+++ lib/simple3d/Vector3.java
@@ -0,0 +1,70 @@
+package simple3d;
+
+public final class Vector3 {
+    public final double x, y, z;
+
+    public Vector3(double x, double y, double z, double w) {
+        this.x = x / w;
+        this.y = y / w;
+        this.z = z / w;
+    }
+
+    public Vector3(double x, double y, double z) {
+        this(x, y, z, 1);
+    }
+
+    public static Vector3 zero() {
+        return new Vector3(0, 0, 0);
+    }
+
+    public static Vector3 x() {
+        return new Vector3(1, 0, 0);
+    }
+
+    public static Vector3 y() {
+        return new Vector3(0, 1, 0);
+
+    }
+
+    public static Vector3 z() {
+        return new Vector3(0, 0, 1);
+    }
+
+    public double magnitude() {
+        return Math.sqrt(x * x + y * y + z * z);
+    }
+
+    public Vector3 normalized() {
+        double mag = magnitude();
+        return new Vector3(x / mag, y / mag, z / mag);
+    }
+
+    public Vector3 plus(Vector3 p) {
+        return new Vector3(x + p.x, y + p.y, z + p.z);
+    }
+
+    public Vector3 minus(Vector3 p) {
+        return new Vector3(x - p.x, y - p.y, z - p.z);
+    }
+
+    public Vector3 times(double n) {
+        return new Vector3(x * n, y * n, z * n);
+    }
+
+    public double dot(Vector3 p) {
+        return x * p.x + y * p.y + z * p.z;
+    }
+
+    public Vector3 cross(Vector3 p) {
+        double tmpX = y * p.z - z * p.y;
+        double tmpY = x * p.z - z * p.x;
+        double tmpZ = x * p.y - y * p.x;
+
+        return new Vector3(tmpX, tmpY, tmpZ);
+    }
+
+    @Override
+    public String toString() {
+        return "Vector3{" + "x=" + x + ", y=" + y + ", z=" + z + '}';
+    }
+}
